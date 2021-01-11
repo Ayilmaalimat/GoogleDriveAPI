c164625fa12f2116a0516ce1f6b931a78dbde2b0
Zhantai <zhantai.ismailov@gmail.com>
2021-01-11 14:20:17 +0600

    intead of functions, i used OOP

---
+++ GoogleAPI.py
@@ -0,0 +1,70 @@
+from Google import Create_Service
+from googleapiclient.http import MediaIoBaseDownload, MediaFileUpload
+import os
+import mimetypes
+import io
+
+CLIENT_SECRET_FILE = 'credentials.json'
+API_NAME = 'drive'
+API_VERSION = 'v3'
+SCOPES = ['https://www.googleapis.com/auth/drive']
+
+service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+
+
+class GoogleAPI():
+    def create(self, my_folders):
+        for folders in my_folders:
+            file_metadata = {
+                'name': folders,
+                'mimeType': 'application/vnd.google-apps.folder'
+            }
+
+        service.files().create(body=file_metadata).execute()
+
+    def download(self, file_ids, file_names):
+
+        for file_id, file_name in zip(file_ids, file_names):
+            request = service.files().get_media(fileId=file_id)
+
+            fh = io.BytesIO()
+            downloader = MediaIoBaseDownload(fd=fh, request=request)
+
+            done = False
+            while not done:
+                status, done = downloader.next_chunk()
+                print('Download progress {}'.format(status.progress() * 100))
+
+            fh.seek(0)
+
+            with open(os.path.join('./downloaded', file_name), 'wb') as f:
+                f.write(fh.read())
+                f.close()
+
+    def upload(self, folder_id, file_names, mime_types):
+        for file, mime_type in zip(file_names, mime_types):
+            file_metadata = {
+                'name': file,
+                'parents': [folder_id],
+            }
+
+            media = MediaFileUpload(
+                './uploadfiles/{0}'.format(file), mimetype=mime_type)
+
+            service.files().create(
+                body=file_metadata,
+                media_body=media,
+                fields='id',
+            ).execute()
+
+    def copy(self, file_id, folder_ids):
+        for folder_id in folder_ids:
+            file_metadata = {
+                'parents': [folder_id],
+                'starred': True
+            }
+
+            service.files().copy(
+                fileId=file_id,
+                body=file_metadata
+            ).execute()
--- main.py
+++ main.py
@@ -1,32 +1,21 @@
-from logic import create, download, upload, copy_items
+from GoogleAPI import GoogleAPI
 
-# create(['mysecond'])
-""" function "create" require the list of names """
+google = GoogleAPI()
+
+# google.create(["my_folders"])
+# google.create(['name']) get a list with a name
 
 
-#download(['1JDi1oi3Rl-yaAEfCvFiQSJ0lR1qD7L5L','1ypw09y9AzREo8RNwuVfoFGfJGKVIDZKH'], ['photo.jpeg', 'test.csv'])
+# google.upload('1rVNBk2oTNDGruCJFFmSLoY1TiXs6aAP1', [
+#               'd7da75accefe0e3b445d3cd078429b074d9cd7a5.jpg', 'upload.csv'], ['image/jpeg', 'text/csv'])
+# google.upload() 1 argument - id of the folder, 2 argument - list of the names, 3 argument - list of the mime types
 
-"""
-    function 'download' get two lists. 
-    first list contains ids_of_the_items, 
-    second list contains names_of_the_items with its file extension
-"""
 
-# upload('1pcPaSvzgJBgHnSVe-nUymyNQNFlg6Fqw',
-#        ['d7da75accefe0e3b445d3cd078429b074d9cd7a5.jpg', 'upload.csv'], ['image/jpeg', 'text/csv'])
+# google.download(['12-ZygtZbKP-K4-bm2PGB8LTJmpxaTPLr',
+#                  '1cAeSWgkogytW_v-qT_TRapILB2uAPfgU'], ['photo.jpeg', 'test.csv'])
+# google.download() 1 argument - list of the ids of the items, 2 argument - list with the names of the files with extensions
 
-"""
-    fucntion 'upload' uploads file on google discs
-    first argument should be the id of the folder where you want to upload the files
-    second argument is a list of the names with extensions of the files that you want to upload
-    third argument is also the list but with the mime types of the file ( you can find it on https://learndataanalysis.org/commonly-used-mime-types/)
-"""
 
-# #copy_items('13wwrXV-RyDB6pEyaJ5Q6iXNB1vZhMy4hGpYKsFO5ufQ',
-#            ['1pcPaSvzgJBgHnSVe-nUymyNQNFlg6Fqw', '1SYEfS-aqb6AaTu2a-r0F3x5NVe02OYWz'])
-
-"""
-    function 'copy_items' copy files
-    first argument get an id of the file you want to copy
-    second argument get a list of file's ids where you want to copy
-"""
+# google.copy('1rDd56cUcJkicyathIjx7M8UGVT-wd_-KcY-FjnqgjGk',
+#             ['10pumJciJBybm18vRBozFoOSIahVWvex9', '1rVNBk2oTNDGruCJFFmSLoY1TiXs6aAP1'])
+# google.copy() 1 argument - id of the file, 2 argument - list of the file's ids where you want to copy
